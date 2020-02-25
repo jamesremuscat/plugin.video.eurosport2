a710be34e79d2c6553d5f8973307d57af0d7236a
James Muscat <jamesremuscat@gmail.com>
2020-02-25 16:52:13 +0000

    Start playing around with the schedule.

--- resources/lib/eurosport.py
+++ resources/lib/eurosport.py
@@ -16,6 +16,18 @@ class Eurosport(object):
             'cookie': 'st={}'.format(token)
         }
 
+    def collection(self, collection_id, params={}):
+        args = ['{}={}'.format(k, v) for k, v in params.items()]
+        return EurosportResponse(
+            self.session.get(
+                '{}/cms/collections/{}?{}'.format(
+                    ROOT_URL,
+                    collection_id,
+                    '&'.join(args)
+                )
+            ).json()
+        )
+
     def schedule(self):
         return EurosportResponse(
             self.session.get(
@@ -36,6 +48,16 @@ class EurosportResponse(object):
     def __init__(self, data):
         self._data = data
 
+    def find_alias(self, alias_name):
+        candidates = list(filter(
+            lambda o: o.get('attributes', {}).get('alias') == alias_name,
+            self._data.get('included', [])
+        ))
+        if len(candidates) > 0:
+            return candidates[0]
+        else:
+            return None
+
     def videos(self, onlyAvailable=True):
 
         def filterMethod(o):
@@ -49,9 +71,11 @@ class EurosportResponse(object):
             if len(availability) > 0:
                 av_window = availability[0]
                 av_start = parse_date(av_window['playableStart'])
-                av_end = parse_date(av_window['playableEnd'])
+                # NB Videos continue to be available after 'playableEnd'... *shrug*
                 now = datetime.datetime.now(tz.tzutc())
-                return av_start <= now <= av_end
+                is_available = av_start <= now
+                o['attributes']['isAvailable'] = is_available
+                return is_available
 
             return False
 
@@ -76,3 +100,28 @@ class EurosportResponse(object):
         if len(wanted_images) > 0:
             return wanted_images[0]['attributes'].get('src')
         return None
+
+    def get_included_by_id(self, id):
+        candidates = list(filter(
+            lambda o: o.get('id') == id,
+            self._data.get('included', [])
+        ))
+        if len(candidates) > 0:
+            return candidates[0]
+        else:
+            return None
+
+    def get_relations(self, relation_type):
+        relationships = list(
+            filter(
+                lambda r: r.get('type') == relation_type,
+                self._data.get('relationships')
+            )
+        )
+
+        return list(
+            map(
+                lambda r: self.get_included_by_id(r['id']),
+                relationships
+            )
+        )
--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import routing
+import dateutil
 import logging
 import requests
+import routing
 import simplejson
 import xbmcaddon
+
+from datetime import date, timedelta
 from resources.lib import kodiutils, kodilogging
 from resources.lib.eurosport import Eurosport
 from xbmcgui import ListItem
@@ -16,14 +19,83 @@ logger = logging.getLogger(ADDON.getAddonInfo('id'))
 kodilogging.config()
 plugin = routing.Plugin()
 
+setContent(plugin.handle, 'videos')
+
 token = ADDON.getSetting('eurosport-token')
 logger.info('Using token: {}'.format(token))
 e = Eurosport(token)
 
 
-def video_sort_key(video):
+def add_static_index_items(schedule_id):
+    schd_item = ListItem('Schedule')
+    addDirectoryItem(
+        plugin.handle,
+        plugin.url_for(
+            list_schedule,
+            schedule_id=schedule_id
+        ),
+        schd_item,
+        True
+    )
+
+
+def listitem_from_video(schedule, video, include_start_time=False):
     attrs = video['attributes']
+
     if attrs.get('broadcastType') == 'LIVE':
+        title = 'Live: {}'.format(attrs.get('name'))
+    else:
+        title = attrs.get('name')
+
+    start_time = attrs.get('scheduleStart')
+    if include_start_time and start_time:
+        parsed_start_time = dateutil.parser.parse(start_time)
+        title = '{} {}'.format(
+            parsed_start_time.strftime('%H:%M'),
+            title.encode('utf-8')
+        )
+
+    item = ListItem(title)
+
+    images = video.get(
+        'relationships', {}
+    ).get(
+        'images', {}
+    ).get(
+        'data', []
+    )
+
+    if len(images) > 0:
+        image_url = schedule.get_image_url(images[0]['id'])
+        item.setArt({
+            'thumb': image_url,
+            'icon': image_url
+        })
+
+    is_available = attrs.get('isAvailable', False)
+    is_playable_string = 'true' if is_available else 'false'
+    if not is_available:
+        title = '[COLOR dimgray]{}[/COLOR]'.format(title)
+
+    labels = {
+        'title': title,
+        'plot': attrs.get('description'),
+        'premiered': attrs.get('scheduleStart'),
+        'mediatype': 'video'
+    }
+
+    item.setInfo('Video', labels)
+
+    item.setProperty('IsPlayable', is_playable_string)
+    item.setProperty('inputstreamaddon', 'inputstream.adaptive')
+    item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+
+    return item
+
+
+def video_sort_key(video, liveFirst=True):
+    attrs = video['attributes']
+    if liveFirst and attrs.get('broadcastType') == 'LIVE':
         # sort live videos first
         return -1
     return attrs.get('scheduleStart')
@@ -32,48 +104,16 @@ def video_sort_key(video):
 @plugin.route('/')
 def index():
 
-    setContent(plugin.handle, 'videos')
-
     schedule = e.schedule()
+
+    schd_obj = schedule.find_alias('schedule')
+    if schd_obj:
+        add_static_index_items(schd_obj['id'])
+
     videos = schedule.videos()
 
     for video in sorted(videos, key=video_sort_key):
-        attrs = video['attributes']
-
-        if attrs.get('broadcastType') == 'LIVE':
-            title = 'Live: {}'.format(attrs.get('name'))
-        else:
-            title = attrs.get('name')
-
-        item = ListItem(title)
-
-        images = video.get(
-            'relationships', {}
-        ).get(
-            'images', {}
-        ).get(
-            'data', []
-        )
-
-        if len(images) > 0:
-            image_url = schedule.get_image_url(images[0]['id'])
-            item.setArt({
-                'thumb': image_url,
-                'icon': image_url
-            })
-
-        labels = {
-            'title': title,
-            'plot': attrs.get('description'),
-            'premiered': attrs.get('scheduleStart'),
-            'mediatype': 'video'
-        }
-
-        item.setInfo('Video', labels)
-
-        item.setProperty('IsPlayable', 'true')
-        item.setProperty('inputstreamaddon', 'inputstream.adaptive')
-        item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+        item = listitem_from_video(schedule, video)
 
         addDirectoryItem(
             plugin.handle,
@@ -85,6 +125,60 @@ def index():
             False,
             len(videos)
         )
+
+    endOfDirectory(plugin.handle)
+
+
+@plugin.route('/schedule')
+def list_schedule():
+    schedule_id = plugin.args['schedule_id'][0]
+
+    if 'day' in plugin.args:
+        day = plugin.args['day'][0]
+
+        schedule = e.collection(
+            schedule_id,
+            {
+                'include': 'default',
+                'pf[day]': day
+            }
+        )
+
+        videos = schedule.videos(onlyAvailable=False)
+
+        for video in sorted(videos, key=lambda v: video_sort_key(v, False)):
+            item = listitem_from_video(schedule, video, include_start_time=True)
+
+            addDirectoryItem(
+                plugin.handle,
+                plugin.url_for(
+                    play_video,
+                    id=video['id']
+                ),
+                item,
+                False,
+                len(videos)
+            )
+    else:
+        today = date.today()
+        for day_idx in range(14):
+            day_delta = day_idx - 7
+            day = today + timedelta(days=day_delta)
+
+            day_formatted = day.strftime("%Y-%m-%d")
+            item = ListItem(day_formatted)
+            item.select(day == today)
+
+            addDirectoryItem(
+                plugin.handle,
+                plugin.url_for(
+                    list_schedule,
+                    schedule_id=schedule_id,
+                    day=day_formatted
+                ),
+                item,
+                True
+            )
 
     endOfDirectory(plugin.handle)
 
